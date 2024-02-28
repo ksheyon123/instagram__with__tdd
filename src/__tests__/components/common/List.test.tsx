eee42c9e42f7371b119c9b5c5b1ac64d81b31e6d
kangseohyeon <kshyeon123@gmail.com>
2024-02-28 16:47:08 +0900

    :test:write > add testing cases

--- src/__tests__/components/common/List.test.tsx
+++ src/__tests__/components/common/List.test.tsx
@@ -14,11 +14,23 @@ describe("List Component", () => {
     expect(container.children).toHaveLength(2);
   });
 
-  it("is clicked by user", () => {
+  it("which is clicked the hidden child component is opened.", () => {
     const CHILD_TEXT = "HI";
     const CHILD_COMPONENT = <div>{CHILD_TEXT}</div>;
     const { getByText } = render(<></>);
     const component = getByText(CHILD_TEXT);
     expect(component).toHaveLength(2);
   });
+
+  it("with description", () => {});
+
+  it("if the user click the item of list twice, it will be closed", () => {});
+});
+
+describe("List component with the remove button", () => {
+  it("if the user click the btn, item will be removed.", () => {});
+});
+
+describe("List component with the chk button", () => {
+  it("if the user click the btn, item will be high-lighted.", () => {});
 });
