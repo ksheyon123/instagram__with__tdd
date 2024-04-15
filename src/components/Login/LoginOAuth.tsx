f2f7a00e217efcbdc30e9eacd87d5b62d0fb60a7
kangseohyeon <kshyeon123@gmail.com>
2024-04-16 08:50:13 +0900

    :sparkles:feat > add o auth 2.0

--- src/components/Login/LoginOAuth.tsx
+++ src/components/Login/LoginOAuth.tsx
@@ -1,3 +1,22 @@
+import ProfileIcon from "@/assets/icons/profile_icon.jpeg";
+import Image, { StaticImageData } from "next/image";
+import { Button } from "@/components/Common/Button";
+import { List } from "@/components/Common/List";
+
+type Profile = {
+  id: number;
+  name: string;
+  icon: StaticImageData;
+};
+
+const PROFILES: Profile[] = [
+  {
+    id: 1,
+    name: "kshyeon123",
+    icon: ProfileIcon,
+  },
+];
+
 const LoginOAuth: React.FC = () => {
   return (
     <div className="flex flex-col justify-center max-w-[350px] w-full">
@@ -8,8 +27,28 @@ const LoginOAuth: React.FC = () => {
       </div>
       <div>
         <div className="p-4">
-          <div className="border-t pt-3 border-gray239"></div>
+          <div className="flex flex-col border-t pt-3 border-gray239">
+            <List
+              items={PROFILES}
+              child={(data) => <OAuthProfile {...data} />}
+            />
+          </div>
         </div>
+      </div>
+    </div>
+  );
+};
+
+const OAuthProfile: React.FC<Profile> = (props) => {
+  const { id, name, icon } = props;
+  return (
+    <div key={id} className="flex flex-row pb-3 items-center">
+      <div className="w-8 h-8 mr-3">
+        <Image src={icon} alt="profile" />
+      </div>
+      <div className="grow">{name}</div>
+      <div className="w-[68px] h-8">
+        <Button name="로그인" onClick={() => {}} />
       </div>
     </div>
   );
